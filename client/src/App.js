01248b91d8da50119b884d0393800f87f7db312b
fdhernandez <rodriguezhernandez0406@gmail.com>
2022-03-01 20:01:14 -0800

    adding method

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './Pages/Home';
 import Test from './Pages/Test';
+import ThemeContextProvider from "../../contexts/ThemeContext";
 
 function App() {
   return (  
+    <ThemeContextProvider>
     <Router>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/test" element={<Test/>}/>
       </Routes>
     </Router>
+    </ThemeContextProvider>
   );
 }
 
