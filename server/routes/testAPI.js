be1ae01c76353d12f5a96f53c677a939c80279fb
Aaron Gonzalez <31080326+arxeous@users.noreply.github.com>
2022-03-04 14:42:59 -0800

    basic API implementations

--- server/routes/testAPI.js
+++ server/routes/testAPI.js
@@ -1,5 +1,21 @@
 const express = require('express')
 const jsdom = require('jsdom')
 const {parse} = require('himalaya')
-const router  = express.Router
+const router  = express.Router();
 const {JSDOM} = jsdom;
+
+router.get('/', (req, res) => {
+    res.status(200).json({"users" : ["userOne", "userTwo", "userThree", "userFour"]});
+});
+
+router.post('/', (req,res) => {
+    const url = req.body;
+    JSDOM.fromURL(url.url).then(dom => {
+        const html = dom.serialize();
+        const json = parse(html);
+        console.log(json[0]["children"]);
+    })
+    res.send(`User sent url of ${url.url}`);
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+const testRoutes = require('./routes/testAPI')
 const app = express();
+const PORT = 5000;
 
-// Returns this simple json of users.
-app.get("/api", (req, res) =>
-    res.json({"users" : ["userOne", "userTwo", "userThree"] })
-)
+
+app.use(bodyParser.json());
+app.use('/test',testRoutes);
 
 // Server listens on port 5000, app runs on 3000.
 app.listen(5000, () => {console.log("server started on port 5000")})
